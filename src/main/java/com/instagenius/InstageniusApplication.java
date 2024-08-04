713bb8820ed8b8f508c7721bd79ac552c6944667
wrobel2131 <wrobelek2131@wp.pl>
2024-08-04 13:52:29 +0200

    Initial commit to set parent project

---
+++ src/main/java/com/instagenius/InstageniusApplication.java
@@ -0,0 +1,13 @@
+package com.instagenius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstageniusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstageniusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/instagenius/InstageniusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.instagenius;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstageniusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
