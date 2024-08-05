36115fc15b06dc985a006db6c47eb16653fe12a7
wrobel2131 <wrobelek2131@wp.pl>
2024-08-06 01:45:36 +0200

    feat: Added empty config service, modified parentm pom.xml(will change during development)

--- config-service/src/test/java/com/instagenius/configservice/ConfigServiceApplicationTests.java
+++ config-service/src/test/java/com/instagenius/configservice/ConfigServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.instagenius;
+package com.instagenius.configservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class InstageniusApplicationTests {
+class ConfigServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
