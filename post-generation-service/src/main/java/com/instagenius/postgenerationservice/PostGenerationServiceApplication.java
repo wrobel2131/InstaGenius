ff09a095319c7417fad155bb6cad9e7e13142342
wrobel2131 <wrobelek2131@wp.pl>
2024-08-06 22:26:24 +0200

    feat: Added new service for generating posts with openai, added new configuration file for this service and edited workflow file to build and test new service

---
+++ post-generation-service/src/main/java/com/instagenius/postgenerationservice/PostGenerationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.instagenius.postgenerationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostGenerationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostGenerationServiceApplication.class, args);
+	}
+
+}
---
+++ post-generation-service/src/test/java/com/instagenius/postgenerationservice/PostGenerationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.instagenius.postgenerationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostGenerationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
