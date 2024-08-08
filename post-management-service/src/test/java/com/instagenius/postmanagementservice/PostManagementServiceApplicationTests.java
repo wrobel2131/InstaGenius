db91e0526c7e77f4f44aa3f898ce4a63d3d1b09e
wrobel2131 <wrobelek2131@wp.pl>
2024-08-08 21:34:57 +0200

    feat: initialized post management service

---
+++ post-management-service/src/test/java/com/instagenius/postmanagementservice/PostManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.instagenius.postmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
