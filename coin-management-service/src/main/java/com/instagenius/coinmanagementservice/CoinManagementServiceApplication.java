4073ee196c23c89a5526ad2a3354eb243c7e920f
wrobel2131 <wrobelek2131@wp.pl>
2024-09-11 23:31:04 +0200

    feat:Added initial configuration for coin management service and created database for service

---
+++ coin-management-service/src/main/java/com/instagenius/coinmanagementservice/CoinManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.instagenius.coinmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinManagementServiceApplication.class, args);
+	}
+
+}
---
+++ coin-management-service/src/test/java/com/instagenius/coinmanagementservice/CoinManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.instagenius.coinmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
