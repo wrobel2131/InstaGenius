53a09688d6d57594168ada7731d8a8cd3751bbb8
wrobel2131 <wrobelek2131@wp.pl>
2024-11-20 17:14:40 +0100

    feat: Added new maven project as a keycloak provider, which is event listener

---
+++ keycloak-event-listener/src/main/java/com/instagenius/user/UserEventListenerProvider.java
@@ -0,0 +1,45 @@
+package com.instagenius.user;
+
+import lombok.RequiredArgsConstructor;
+import org.keycloak.events.Event;
+import org.keycloak.events.EventListenerProvider;
+import org.keycloak.events.EventType;
+import org.keycloak.events.admin.AdminEvent;
+import org.keycloak.models.KeycloakSession;
+
+import java.util.logging.Logger;
+
+@RequiredArgsConstructor
+public class UserEventListenerProvider implements EventListenerProvider {
+    private static final Logger logger = Logger.getLogger(UserEventListenerProvider.class.getName());
+    private final KeycloakSession keycloakSession;
+
+    @Override
+    public void onEvent(Event event) {
+        EventType eventType = event.getType();
+        logger.info("Event occurred: " + event.getType());
+
+        switch (eventType) {
+            case REGISTER -> logger.info("Register event");
+            case DELETE_ACCOUNT -> logger.info("Delete account event");
+            case USER_DISABLED_BY_PERMANENT_LOCKOUT -> logger.info("Permanent lockout user disabled event");
+            case USER_DISABLED_BY_TEMPORARY_LOCKOUT -> logger.info("Temporary lockout user disabled event");
+            case UPDATE_EMAIL -> logger.info("Update email event");
+            case UPDATE_PROFILE -> logger.info("Update profile event");
+            case VERIFY_EMAIL -> logger.info("Verify email event");
+            case VERIFY_PROFILE -> logger.info("Verify profile event");
+            default -> logger.info("Other event occurred");
+        }
+
+    }
+
+    @Override
+    public void onEvent(AdminEvent adminEvent, boolean b) {
+        // Do nothing
+    }
+
+    @Override
+    public void close() {
+        // Do nothing
+    }
+}
---
+++ keycloak-event-listener/src/main/java/com/instagenius/user/utils/EventHandler.java
@@ -0,0 +1,36 @@
+package com.instagenius.user.utils;
+
+import com.instagenius.user.dto.UserDto;
+import lombok.experimental.UtilityClass;
+import org.keycloak.events.Event;
+import org.keycloak.events.EventType;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.RealmModel;
+import org.keycloak.models.UserModel;
+
+import java.util.Map;
+
+@UtilityClass
+public class EventHandler {
+
+    public void handleRegisterEvent(Event event, KeycloakSession keycloakSession) {
+        EventType eventType = event.getType();
+
+        if(eventType.equals(EventType.REGISTER)) {
+            String userId = event.getUserId();
+            String realmId = event.getRealmId();
+            UserDto user = getUser(keycloakSession, userId, realmId);
+
+            //TODO perform API call to userService to create user
+
+        }
+    }
+
+
+    private UserDto getUser(KeycloakSession keycloakSession, String userId, String realmId) {
+        RealmModel realm = keycloakSession.realms().getRealm(realmId);
+        UserModel user = keycloakSession.users().getUserById(realm, userId);
+        return new UserDto(user.getId(), user.getEmail(), user.getFirstName(), user.getLastName(),
+                user.isEmailVerified(), user.isEnabled(), user.getCreatedTimestamp());
+    }
+}
